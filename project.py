035be5925fcfe5536cb894d84061259d234fd95c
NagindarSingh25 <162889988+NagindarSingh25@users.noreply.github.com>
2024-05-11 14:43:15 +0530

    All the files are added

---
+++ project.py
@@ -0,0 +1,19 @@
+import streamlit as st
+st.title('MPG ML PROJECT')
+displacement = st.number_input('enter a value for displacement',
+value=300, placeholder='enter a value for displcement')
+Horsepower = st.number_input('enter a value for horsepower',
+value=130, placeholder='enter a value for horsepower')
+
+weight = st.number_input('enter a value for weight',
+value=3000, placeholder='enter a value for weight')
+Acceleration = st.number_input('enter a value for Acceleration',
+value=12, placeholder='enter a value for Acceleration')
+
+import pickle
+
+loaded_model = pickle.load(open('mpg_regression.sav','rb'))
+prediction = loaded_model.predict([[displacement,Horsepower,weight,Acceleration]])
+st.subheader(f'predicted mpg value for above parametre is {prediction[0]}')
+
+st.write(prediction)
